4d790da8629269fe1b241c2ff22e627bed0cde64
krishna <krishna@azrisolutions.com>
2018-06-11 12:18:41 +0530

    installed deps

---
+++ mix.exs
@@ -0,0 +1,38 @@
+defmodule XMLParser.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :xmlparser,
+      version: "0.1.0",
+      elixir: "~> 1.0",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package(),
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:erlsom, github: "willemdj/erlsom"},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+    ]
+  end
+
+  defp package do
+    [
+      name: :xmlparser,
+      files: ~w(lib mix.exs README.md LICENSE VERSION),
+      maintainers: ["Krishna Chakrapani"],
+      links: %{"GitHub" => "https://github.com/devinus/poison"},
+    ]
+  end
+end
