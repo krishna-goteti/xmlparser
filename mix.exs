f8747a39796d71d0ebe4359a756d393ae851cfe0
Krishna Goteti <krishna@azrisolutions.com>
2018-09-10 18:31:32 +0530

    changed versions of deps

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule XMLParser.MixProject do
   defp deps do
     [
       {:erlsom, github: "willemdj/erlsom"},
-      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
     ]
   end
 
